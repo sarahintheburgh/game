e44020b92c60aa0e05dee47bd933c42febc189e2
sarahintheburgh <46507021+sarahintheburgh@users.noreply.github.com>
2019-02-05 20:35:41 -0500

    Add files from lab

---
+++ controller.js
@@ -0,0 +1,12 @@
+document.body.onkeydown = function( e ) {
+  37: 'left',
+  39: 'right',
+  40: 'down',
+  38: 'rotate',
+  32: 'drop'
+};
+
+  if (typeof keys[ e.keyCode] != 'undefined'){
+    keyPress( keys[e.keyCode]);
+    render();
+  };
